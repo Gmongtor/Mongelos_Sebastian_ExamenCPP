d90bdacdfd124fb59a92e7a1bbe39212fb5912c2
Gmongtor <gmongtor@myuax.com>
2023-12-05 11:51:06 +0100

    commit 5 añadimos la variante junto a un bucle que perimitira imprimir un valor u otro dependiendo del tipo de valor al que corresponda.

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <variant>
 #include <string>
+using namespace std;
+using Value = variant<int, double, string>;
+Value getValue(const string& s) {
+ if (s == "int") {
+    return 10;
+    } else if (s == "double") {
+        return 3.14;
+    } else if (s == "string") {
+        return "Hola mundo";
+    }
+    throw runtime_error("Tipo de dato no soportado");
+ }
+int main() {
+    try {
+        Value v = getValue("int");
+        cout << get<int>(v) << endl;
+        v = getValue("double");
+        cout << get<double>(v) << endl;
+        v = getValue("string");
+        cout << get<string>(v) << endl;
+        v = getValue("char");
+        cout << get<string>(v) << endl;
+    } catch (const runtime_error &e) {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
