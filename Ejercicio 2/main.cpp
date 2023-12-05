0fefcfea5e1ba8fb85c39589babec1a62ca0d596
Gmongtor <gmongtor@myuax.com>
2023-12-05 11:08:14 +0100

    Commit 2 añadimos la clase

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -2,3 +2,18 @@
 #include <string>
 #include <map>
 using namespace std;
+class Environment {
+private:
+    map<std::string, int> symbolTable;
+public:
+    bool insert (const pair<string,int>& val) {
+        return symbolTable.insert(val).second;
+    }
+    int getSymbol(const string& key) {
+        auto it = symbolTable.find(key);
+        if (it != symbolTable.end()) {
+            return it->second;
+        }
+        throw runtime_error("Simbolo no encontrado");
+    }
+};
