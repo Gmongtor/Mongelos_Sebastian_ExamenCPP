4d11e8ce190eff332914257684422c20063a5a8c
Gmongtor <gmongtor@myuax.com>
2023-12-05 11:57:25 +0100

    Commit 6 añadimos el main para que se pueda imprimir correctamente

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -11,5 +11,25 @@ class TaskList {
 private:
     vector<Task> tasks;
 public:
-
+void addTask(const Task& task) {
+        tasks.push_back(task);
+    }
+    void removeTask(int index) {
+        tasks.erase(tasks.begin() + index);
+    }
+    void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            cout << i << ". " << tasks[i].description << endl;
+        }
+    }
 };
+int main() {
+    TaskList taskList;
+    taskList.addTask(Task("Comprar leche"));
+    taskList.addTask(Task("Comprar pan"));
+    taskList.addTask(Task("Comprar huevos"));
+    taskList.printTasks();
+    taskList.removeTask(1);
+    taskList.printTasks();
+    return 0;
+}
