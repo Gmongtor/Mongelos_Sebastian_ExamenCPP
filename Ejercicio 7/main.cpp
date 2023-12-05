86d127f9105359e0d876fe32811420155c1c1d05
Gmongtor <gmongtor@myuax.com>
2023-12-05 12:07:06 +0100

    Commit 7 clase environment

--- Ejercicio 7/main.cpp
+++ Ejercicio 7/main.cpp
@@ -6,4 +6,11 @@ class Environment {
 public:
     string name;
     map<std::string, string> properties;
+    Environment(string name) : name(name) {}
+    void displayEnvironment() {
+        cout << "Environment: " << name << endl;
+        for (auto& pair : properties) {
+            cout << pair.first << " = " << pair.second << endl;
+        }
+    }
 };
