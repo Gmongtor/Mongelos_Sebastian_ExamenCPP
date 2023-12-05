493a1cb52a14f9eb28571fd5e4dc88d736caa942
Gmongtor <gmongtor@myuax.com>
2023-12-05 11:36:54 +0100

    Commit 4 creamos un void ProcesoDeRiesgo que solo pueda funcionar con numeros positivos, menores de 100 y que no sea 0. En caso de error saltara su respectivo error.

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <string>
+#include <stdexcept>
+void ProcesoDeRiesgo(int n) {
+    if (n < 0) {
+        throw std::runtime_error("El numero no puede ser negativo");
+    } else if (n == 0) {
+        throw std::runtime_error("El numero no puede ser cero");
+    } else if (n > 100) {
+        throw std::runtime_error("El numero no puede ser mayor a 100");
+    }
+    std::cout << "Procesamiento completado con el valor: " << std::endl;
 }
