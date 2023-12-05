51a83ba7ea8a360f4ee1613112ce8e6748183d17
Gmongtor <gmongtor@myuax.com>
2023-12-05 11:56:36 +0100

    Commit 6 creamos clase task y posteriormente clase tasklist

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
+using namespace std;
+class Task {
+public:
+    string description;
+    Task(string description) : description(description) {}
+};
+class TaskList {
+private:
+    vector<Task> tasks;
+public:
 
+};
