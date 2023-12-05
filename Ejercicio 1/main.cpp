c5e431cfab9cf4a2495f1e4823d4c470ee3460c7
Gmongtor <gmongtor@myuax.com>
2023-12-05 10:55:24 +0100

    Commit 1 codigo para obtener el valor y en caso contrario poner simbolo no encontrado

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <string>
 #include <map>
+using namespace std;
 class Environment {
 private:
-
+    map<std::string, int> symbolTable;
+public:
+    void addSymbol(const string& key, int value) {
+        symbolTable[key] = value;
+    }
+    int getSymbol(const string& key) {
+        auto it = symbolTable.find(key);
+        if (it != symbolTable.end()) {
+            return it->second;
+        }
+        throw runtime_error("Simbolo no encontrado");
+    }
 };
