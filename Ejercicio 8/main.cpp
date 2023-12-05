73982f5ad2e3058c5d83c8938bce5ee35043a355
Gmongtor <gmongtor@myuax.com>
2023-12-05 12:21:33 +0100

    Commit 8 definimos los factoriales

--- Ejercicio 8/main.cpp
+++ Ejercicio 8/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-long long factorial(int n) {
-    if (n == 0) return 1;
-    return n * factorial(n - 1);
+long long factorialIterativo(int n) {
+    long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+long long factorialRecursivo(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorialRecursivo(n - 1);
+}
+long long factorialConCache(int n) {
+    static long long cache[1000] = {0};
+    if (n == 0) {
+        return 1;
+    }
+    if (cache[n] != 0) {
+        return cache[n];
+    }
+    cache[n] = n * factorialConCache(n - 1);
+    return cache[n];
 }
